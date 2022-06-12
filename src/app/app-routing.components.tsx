8361c2534d02ad490687038be175ce78a2a358cd
Janbets <janbeeangeles@gmail.com>
2022-06-11 23:46:27 -0400

    auto

--- src/app/app-routing.components.tsx
+++ src/app/app-routing.components.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { NotFound } from '@app/shared/component/not-found';
 import { MainComponent } from '@components/main/main.component';
+import { SkillsComponent } from '@components/skills/skills.component';
 
 export const AppRoutingComponent = (): React.ReactElement | null => {
   return useRoutes([
     {
       path: '',
       element: <MainComponent />,
+    },
+    {
+      path: 'skills',
+      element: <SkillsComponent />,
     },
 
     /*
