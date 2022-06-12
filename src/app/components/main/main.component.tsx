05622407a4297e38d559069d8230b60abeb1c087
Janbets <janbeeangeles@gmail.com>
2022-06-12 01:12:28 -0400

    no message

--- src/app/components/main/main.component.tsx
+++ src/app/components/main/main.component.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 export const MainComponent = () => {
-  return <div>this is main component test 121</div>;
+  return (
+    <div>
+      this is main component test
+      <Button>
+        <Link to="/skills">Skills</Link>
+      </Button>
+    </div>
+  );
 };
--- src/app/components/skills/skills.component.tsx
+++ src/app/components/skills/skills.component.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 export const SkillsComponent = () => {
-  return <div>this is skills component</div>;
+  return (
+    <div>
+      this is skills component
+      <Button>
+        <Link to="/">back to main</Link>
+      </Button>
+    </div>
+  );
 };
